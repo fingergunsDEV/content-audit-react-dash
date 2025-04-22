9b6835821c5903b17beb8fb8de94c45136878923
Jason Gibson <76972956+fingergunsDEV@users.noreply.github.com>
2025-04-22 14:04:09 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuditDashboard from './components/AuditDashboard';
+
+const App = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 text-gray-900">
+      <AuditDashboard />
+    </main>
+  );
+};
+
+export default App;
